c97d3c0508edc023189dbd65582c110be68689a3
Helio <helios25@gmail.com>
2019-01-20 06:21:44 -0500

    Created first subscription

---
+++ graphql-basics/src/resolvers/Subscription.js
@@ -0,0 +1,13 @@
+export default {
+  count: {
+    subscribe(parent, args, { pubsub }, info) {
+      let count = 0
+
+      setInterval(() => {
+        count++
+        pubsub.publish('count', { count })
+      }, 1000)
+      return pubsub.asyncIterator('count') // asyncIterator takes a channel
+    },
+  },
+}
